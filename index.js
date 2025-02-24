571e0e7794b77913f58a41a70ab841680d0ba0a5
LydiaRefaat22 <138793711+LydiaRefaat22@users.noreply.github.com>
2025-02-24 23:18:25 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-let btn=document.getElementById("button");
-
-window.onscroll = function () {
-    if (window.scrollY >= 350) {
-        btn.style.display = "block";
+let sho=document.getElementsByClassName("shoes")[0];
+let box=document.getElementsByClassName("box")[0];
+sho.onclick=function(){
+    if(box!==""){
+        box="undefined"
+    }
+    let clon=sho.cloneNode(true);
+    box.appendChild(clon);
     
 }
-};
-btn.onclick=function(){
-    scroll({
-        left:0,
-        top:0,
-        behavior:"smooth",
-
-      })
+let sho1=document.getElementsByClassName("shoes")[1];
+let sho2=document.getElementsByClassName("shoes")[2];
+let sho3=document.getElementsByClassName("shoes")[3];
+sho1.onclick=function(){
+    let clon=sho1.cloneNode(true);
+    box.appendChild(clon);
 }
-console.log(btn)
-
- 
-// let btn1=document.getElementById("button1");
-// btn1.onclick=function(){
-//     document.body.style.background="black";
-// }
-
-
+sho2.onclick=function(){
+    let clon=sho2.cloneNode(true);
+    box.appendChild(clon);
+}
+sho3.onclick=function(){
+    let clon=sho3.cloneNode(true);
+    box.appendChild(clon);
+}
